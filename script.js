3439cdb1fe8bfa0b0611ecb7198f50c9da6a78e0
Riffate <riffatestyanto@gmail.com>
2025-04-06 16:08:16 +0700

    UBAHAN GEDEN

--- script.js
+++ script.js
@@ -17,3 +17,12 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  if (sidebar.style.width === "250px") {
+    sidebar.style.width = "0";
+  } else {
+    sidebar.style.width = "250px";
+  }
+}
